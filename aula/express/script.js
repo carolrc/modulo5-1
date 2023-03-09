24a840c3a62873fcd4b49787c08cbec7a4758e05
DevairUva <devair.martins@gmail.com>
2023-03-09 18:54:52 -0300

    rotas

--- aula/express/script.js
+++ aula/express/script.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Express!')
+  res.send('Rota raiz')
+})
+
+app.get('/contato', (req, res) => {
+    res.send('Rota Contato')
 })
 
 app.listen(port, () => {
