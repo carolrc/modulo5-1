585ad8135481ff0f49e3a2b7f6c8ea1add841b1f
carolrc <carol_26_01@hotmail.com>
2023-03-13 18:57:47 -0300

    Update usuarioController.js

--- aula/src/models/controles/usuarioController.js
+++ aula/src/models/controles/usuarioController.js
@@ -1,12 +1,13 @@
 function usuarioController(app){
     app.get('/usuario', exibir);
     function exibir(req, res){
-        res.send('<h1>Devair</h1>')
+        res.send('Exibindo usuarios')
     }
 
     app.post('/usuario', inserir);
     function inserir(req, res){
-        res.send('Rota POST de usuario ativada: tarefa adicionada ao BD')
+        res.send('Inserindo usuarios')
+        console.log(req)
     }
 }
 export default usuarioController;
